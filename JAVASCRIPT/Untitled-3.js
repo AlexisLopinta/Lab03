b1b249b1924fc2936d5006d4966bf525bfb3c304
Alexis Frank Lopinta Ala <alexis.lopinta@tecsup.edu.pe>
2022-09-13 19:24:01 -0500

    Tercera version

---
+++ JAVASCRIPT/Untitled-3.js
@@ -0,0 +1,9 @@
+//arreglo en Javascript
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+//cadena vacia (empty string)
+var z = '';
+console.log(z);
+console.log(typeof z);
